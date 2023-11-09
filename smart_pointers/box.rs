e5628810f8ea82ede8f4f3c001252e614df4018c
Virux <starletgh@gmail.com>
2023-11-09 23:22:31 +0000

    MyBox

--- smart_pointers/box.rs
+++ smart_pointers/box.rs
@@ -1,4 +1,5 @@
 use crate::List::{ Cons, Nil };
+use std::ops::Deref;
 
 enum List {
     Cons(i32, Box<List>),
@@ -10,9 +11,38 @@ enum List {
 //    Nil
 //}
 
+// Defining out own Smart Pointers
+#[derive(Debug)]
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> Self {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+
 fn main() {
-    let b = Box::new(5);
-    println!("b holds: {b}");
+//    let b = Box::new(5);
+    let b = MyBox::new(5);
+    println!("b holds: {:#?}", b);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    //15.2. Treating Smart Pointers Like Regular References with the Deref Trait
+    let x = 5;
+    // let y = &x;
+    // Using Box<T> Like a Reference
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+
 }
