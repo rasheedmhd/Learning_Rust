2eb345c960638a2749d90d779fcb5de0f497aac4
Virux <starletgh@gmail.com>
2023-11-09 00:46:53 +0000

    Using a Box<T> to Store Data on the Heap

--- smart_pointers/box.rs
+++ smart_pointers/box.rs
@@ -1,4 +1,18 @@
+use crate::List::{ Cons, Nil };
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil
+}
+
+//enum List<T> {
+//    Cons(T, List<T>),
+//    Nil
+//}
+
 fn main() {
     let b = Box::new(5);
     println!("b holds: {b}");
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
