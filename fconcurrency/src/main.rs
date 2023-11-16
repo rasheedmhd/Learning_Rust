67669d637aa2a6c65b5b8a178e72d15cbf3dda3b
Virux <starletgh@gmail.com>
2023-11-16 09:43:22 +0000

    this is a dead lock haha

--- fconcurrency/src/main.rs
+++ fconcurrency/src/main.rs
@@ -1,6 +1,8 @@
-use std::thread;
-use std::time::Duration;
-use std::sync::mpsc;
+// use std::thread;
+// use std::time::Duration;
+// use std::sync::mpsc;
+
+use std::sync::Mutex;
 
 fn main() {
     // When the main thread finish running, all threads are closed
@@ -32,48 +34,48 @@ fn main() {
 
     // handle.join().unwrap();
 
-    let (tx, rx) = mpsc::channel();
+    // let (tx, rx) = mpsc::channel();
 
-    let tx1 = tx.clone();
+    // let tx1 = tx.clone();
 
-    thread::spawn( move || {
-        let msg = String::from("Mendeleev Crypto Exchange");
-        let chats = vec![
-                String::from("hi"),
-                String::from("I"),
-                String::from("am"),
-                String::from("buying"),
-                "---------------------".to_string(),
-            ];
-        // tx sends a Result<T, E>
-        // returns Error if rx has been dropped already
-        tx.send(msg).unwrap();
+    // thread::spawn( move || {
+    //     let msg = String::from("Mendeleev Crypto Exchange");
+    //     let chats = vec![
+    //             String::from("hi"),
+    //             String::from("I"),
+    //             String::from("am"),
+    //             String::from("buying"),
+    //             "---------------------".to_string(),
+    //         ];
+    //     // tx sends a Result<T, E>
+    //     // returns Error if rx has been dropped already
+    //     tx.send(msg).unwrap();
 
-        // println!("msg is {}", msg);
+    //     // println!("msg is {}", msg);
 
-        for chat in chats {
-            tx.send(chat).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+    //     for chat in chats {
+    //         tx.send(chat).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
 
 
     // obviously my small crypto startup doesn't work, I give up.
     // The messages are getting sent in a random order, not good for business,
     // I am dumb, I can't fix it. I give up. 
-    thread::spawn( move || {
-        let crypto = vec![
-            String::from("Bitcoin"),
-            String::from("Tether"),
-            String::from("Ethereum"),
-            String::from("Cardano"),
-        ];
+    // thread::spawn( move || {
+    //     let crypto = vec![
+    //         String::from("Bitcoin"),
+    //         String::from("Tether"),
+    //         String::from("Ethereum"),
+    //         String::from("Cardano"),
+    //     ];
 
-        for coin in crypto {
-            tx1.send(coin).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+    //     for coin in crypto {
+    //         tx1.send(coin).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
     // rx blocks main thread and returns a Result<T, E>
     // returns Error if tx has been closed and stopped sending
 
@@ -81,11 +83,24 @@ fn main() {
     // let received_message = rx.recv().unwrap();
     // let received_chats = rx.try_recv().unwrap();
 
-    for chat in rx {
-        println!(">> this user: {:?}", chat );
-    }
+    // for chat in rx {
+    //     println!(">> this user: {:?}", chat );
+    // }
 
     // print!("Message received from spawned thread: {:?}", received_message);
+
+    let n = Mutex::new(5);
+
+    let some_fucker = n.lock().unwrap();
+
+    {
+        let mut num = n.lock()
+        .unwrap();
+        *num = 6;
+        println!(">> n: {:?}", n);
+    }
+
+    println!(">> n: {:?}", n);
 }
 
 
