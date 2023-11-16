ba795b019e219185c390cdd5ccc0c8fa91e147c9
Virux <starletgh@gmail.com>
2023-11-16 21:02:18 +0000

    shared state with Arc

--- fconcurrency/src/main.rs
+++ fconcurrency/src/main.rs
@@ -1,18 +1,37 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
 
-    let n = Mutex::new(5);
+    // let n = Mutex::new(5);
 
-    // let some_fucker = n.lock().unwrap();
+    // // let some_fucker = n.lock().unwrap();
 
-    {
-        let mut num = n.lock().unwrap();
-        *num = 6;
-        println!(">> n: On L99 {:?}", n);
+    // {
+    //     let mut num = n.lock().unwrap();
+    //     *num = 6;
+    //     println!(">> n: On L99 {:?}", n);
+    // }
+
+    // println!(">> n: {:?}", n);
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn( move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
     }
 
-    println!(">> n: {:?}", n);
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
 
 
