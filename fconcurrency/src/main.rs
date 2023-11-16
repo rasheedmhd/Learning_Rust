0eb3aef89147e3c48c2fe794c4bff072f6d8cff4
Virux <starletgh@gmail.com>
2023-11-16 07:04:58 +0000

    threads

--- fconcurrency/src/main.rs
+++ fconcurrency/src/main.rs
@@ -7,6 +7,15 @@ fn main() {
     // regardless to whether they have completed running or not.
     println!("Hello, world, Fearless Concurrency!");
 
+    let e = 11; 
+    let v = vec![1, 2, 3];
+
+    let handle1 = thread::spawn( move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle1.join().unwrap();
+
     let handle = thread::spawn(||
         for i in 2..10 {
             println!("got {i}, by looping in spawned thread.");
@@ -14,12 +23,12 @@ fn main() {
         }
     );
 
-    // handle.join().unwrap();
+    handle.join().unwrap();
 
     for i in 1..5 {
         println!("got {i}, by looping in main thread.");
         thread::sleep(Duration::from_millis(1))
     }
 
-    handle.join().unwrap();
+    // handle.join().unwrap();
 }
