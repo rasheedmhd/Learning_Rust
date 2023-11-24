9a99829384dcc67a13c3e0a3cb3a0134d01c7626
Virux <starletgh@gmail.com>
2023-11-24 22:24:26 +0000

    TcpListener

--- dust/src/main.rs
+++ dust/src/main.rs
@@ -1,3 +1,17 @@
+use std::net::TcpListener;
+const PORT: i32 = 7171;
+const ADDRESS: &str = "127.0.0.1";
+
+
 fn main() {
     println!("Dust Web Server!");
+
+    let listener = TcpListener::bind("127.0.0.1:7171").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("[Dust Web Server]: Open here: {}:{}", ADDRESS, PORT);
+        println!("[Dust Web Server]: Connection established!");
+    }
 }
