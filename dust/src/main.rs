b3d7fb5bfc2eaedcae3fa98035e05f70776be35c
Virux <starletgh@gmail.com>
2023-11-24 22:42:10 +0000

    capturing and HTTP requests

--- dust/src/main.rs
+++ dust/src/main.rs
@@ -1,4 +1,14 @@
-use std::net::TcpListener;
+use std::{
+    io::{
+        prelude::*,
+        BufReader
+    },
+    net::{
+        TcpListener,
+        TcpStream
+    },
+};
+
 const PORT: i32 = 7171;
 const ADDRESS: &str = "127.0.0.1";
 
@@ -12,6 +22,18 @@ fn main() {
         let stream = stream.unwrap();
 
         println!("[Dust Web Server]: Open here: {}:{}", ADDRESS, PORT);
-        println!("[Dust Web Server]: Connection established!");
+        // println!("[Dust Web Server]: Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
