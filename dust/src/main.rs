d9f8eb49c8cbdd56ba618bf795899c494c4b229a
Virux <starletgh@gmail.com>
2023-11-26 23:35:31 +0000

    sleeping

--- dust/src/main.rs
+++ dust/src/main.rs
@@ -1,5 +1,7 @@
 use std::{
     fs,
+    thread,
+    time::Duration,
     io::{
         prelude::*,
         BufReader
@@ -38,11 +40,20 @@ fn handle_connection(mut stream: TcpStream) {
 
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "./index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "./404.html")
-    };
+    // let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    //     ("HTTP/1.1 200 OK", "./index.html")
+    // } else {
+    //     ("HTTP/1.1 404 NOT FOUND", "./404.html")
+    // };
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));   
+            ("HTTP/1.1 200 OK", "./index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "./404.html"),
+    };             
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
@@ -67,5 +78,6 @@ fn handle_connection(mut stream: TcpStream) {
     //         format!("{status_line}\r\nContent-Type: text/html\r\nContent-Length: {length}\r\n\r\n{contents}");
     //     stream.write_all(response.as_bytes()).unwrap();
     // }
+
 }
 
