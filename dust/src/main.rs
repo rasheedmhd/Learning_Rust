7e368d72dcb4193fcae01ca14c1d0091460ff09d
Virux <starletgh@gmail.com>
2023-11-27 09:21:42 +0000

    spawn thread on each request

--- dust/src/main.rs
+++ dust/src/main.rs
@@ -26,10 +26,11 @@ fn main() {
 
         println!("[Dust Web Server]: Open here: {}:{}", ADDRESS, PORT);
         // println!("[Dust Web Server]: Connection established!");
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
-
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     // let http_request: Vec<_> = buf_reader
@@ -58,10 +59,6 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
     // if request_line == "GET / HTTP/1.1" {
     //     let status_line = "HTTP/1.1 200 OK";
     //     let contents = fs::read_to_string("./index.html").unwrap();
@@ -78,6 +75,11 @@ fn handle_connection(mut stream: TcpStream) {
     //         format!("{status_line}\r\nContent-Type: text/html\r\nContent-Length: {length}\r\n\r\n{contents}");
     //     stream.write_all(response.as_bytes()).unwrap();
     // }
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 
 }
 
