618cfecc9df369f2dc4da869b2d22e60cc5a95fc
rasheedmhd <starletgh@gmail.com>
2024-05-01 20:03:22 +0000

    get and loop

--- collections/hashmap.rs
+++ collections/hashmap.rs
@@ -8,4 +8,16 @@ fn main() {
     // all values = same type
     scores.insert("Blue", 11);
     scores.insert("Red", 151);
+
+    // Getting values from a HashMap using their keys
+    // .get() -> Option(Value), if the key doesn't exist
+    // we gracefully handle it by returning 0 [.unwrap_or(0)]
+    let blue_score = scores.get("Blue").copied().unwrap_or(0);
+
+    println!(" blue score: {:#?} ", blue_score);
+
+    // looping through a HashMap to print value pairs
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
 }
