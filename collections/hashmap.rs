6fa69ec602a091a00087ef1cbb8ce9bbff87255f
rasheedmhd <starletgh@gmail.com>
2024-05-01 22:09:10 +0000

    hashmaps

--- collections/hashmap.rs
+++ collections/hashmap.rs
@@ -51,4 +51,33 @@ fn main() {
     // a new value to the key
     scores.entry("Blue").or_insert(56);
     scores.entry("Blu").or_insert(56);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    // for word in text.split_whitespace() {
+    //     let count = map.entry(word).or_insert(0);
+    //     *count += 1;
+    // }
+
+    // Rewriting the for lo0p with a closure
+    for word in text.split_whitespace() {
+        map.entry(word)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+    }
+
+    println!("{:?}", map);
+    // this should be fun.
+    // Convert strings to pig latin. The first consonant of each word is moved to the end of
+    // the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a
+    // vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind t
+    // he details about UTF-8 encoding!
+    //
+    // this should be fun.
+    // Using a hash map and vectors, create a text interface to allow a user to add employee
+    // names to a department in a company. For example, “Add Sally to Engineering” or “Add
+    // Amir to Sales.” Then let the user retrieve a list of all people in a department or all
+    // people in the company by department, sorted alphabetically.
 }
